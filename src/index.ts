08b6ff815310dc81363f8da1be3a11a4e9e2fccd
Jose Hurtado <hello@hurtadojose.com>
2025-06-12 00:14:55 +0200

    index.ts missing paths for icons

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export * from './credentials/AnthropicApi.credentials';
+export * from './credentials/OpenAIApi.credentials';
+export * from './credentials/GoogleApi.credentials';
+export * from './credentials/CustomApi.credentials';
 export * from './nodes/PdfAccessibility/PdfAccessibility.node';
