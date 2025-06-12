6dd0ab5b5c66730a99c7e7062113a7aef72c1256
Jose Hurtado <hello@hurtadojose.com>
2025-06-13 00:01:11 +0200

    Fix package.json repository URL and icon build process for NPM publishing

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@ const { src, dest } = require('gulp');
 
 function buildIcons() {
     // Copy main icon to dist root (required for N8N package discovery)
-    return src('icons/pdf-accessibility.svg').pipe(dest('dist/'));
+    src('icons/pdf-accessibility.svg').pipe(dest('dist/'));
+    
+    // Also copy to icons directory for verification
+    return src('icons/**/*').pipe(dest('dist/icons/'));
 }
 
 exports['build:icons'] = buildIcons;
